5e192eb4c987ab99cd00b7aa9f080c413dea1444
NataliePereira6020 <64098584+NataliePereira6020@users.noreply.github.com>
2020-06-09 18:20:53 +0100

    Upload
    Upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>When is the best time to travel?</h2>
+
+<p>Click the button to find out when the ideal time to start your adventures is.</p>
+
+<p id="demo" style="display:none">NOW!</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
+
+</body>
+</html> 
